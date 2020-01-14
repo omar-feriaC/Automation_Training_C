7ec34b9b5ae6a843831edfe4822f7143b6389780
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2020-01-14 16:05:50 -0600

    adding extra class

---
+++ AutomationFrameworkC/AutomationFrameworkC/Exercise/ICar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFrameworkC.Exercise
+{
+    interface ICar
+    {
+        //Attributes
+        string engine { get; set; }
+        string tires { get; set; }
+        string color { get; set; }
+        string model { get; set; }
+        string plate { get; set; }
+
+        //Methods
+        void fnStart();
+        void fnBrake();
+        void fnParking();
+        void fnCalculateKM();
+
+    }
+}
