5fba9566bb1ffd73aee9ed4f0dacf8715a86d44c
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2020-01-14 16:56:47 -0600

    Adding extra class

--- AutomationFrameworkC/AutomationFrameworkC/Exercise/clsClassicCar.cs
+++ AutomationFrameworkC/AutomationFrameworkC/Exercise/clsClassicCar.cs
@@ -8,26 +8,62 @@ namespace AutomationFrameworkC.Exercise
 {
     class clsClassicCar : ICar
     {
-        public string engine { get; set; }
-        public string tires { get; set; }
-        public string color { get; set; }
-        public string model { get; set; }
-        public string plate { get; set; }
+        //Attributes
+        public string strEngine { get; set; }
+        public string strTires { get; set; }
+        public string strColor { get; set; }
+        public string strModel { get; set; }
+        public string strPlate { get; set; }
 
+        //Constructor
+        public clsClassicCar()
+        {
+            strEngine = "";
+            strTires = "";
+            strColor = "";
+            strModel = "";
+            strPlate = "";
+        }
+
+        public clsClassicCar(string pstrEngine, string pstrTires, string pstrColor, string pstrModel, string pstrPlate)
+        {
+            strEngine = pstrEngine;
+            strTires = pstrTires;
+            strColor = pstrColor;
+            strModel = pstrModel;
+            strPlate = pstrPlate;
+        }
+
+        //Methods
         public void fnBrake()
         {
+            Console.WriteLine("ClassicCar_fnBrake: ");
         }
 
         public void fnCalculateKM()
         {
+            Console.WriteLine("ClassicCar_fnCalculateKM");
         }
 
         public void fnParking()
         {
+            Console.WriteLine("ClassicCar_fnParking");
         }
 
         public void fnStart()
         {
+            Console.WriteLine("ClassicCar_fnStart");
         }
+
+        public void fnDisplayInfo()
+        {
+            Console.WriteLine("strEngine: " + strEngine);
+            Console.WriteLine("strTires: " + strTires);
+            Console.WriteLine("strColor: " + strColor);
+            Console.WriteLine("strModel: " + strModel);
+            Console.WriteLine("strPlate: " + strPlate);
+        }
+
+
     }
 }
--- AutomationFrameworkC/AutomationFrameworkC/Program.cs
+++ AutomationFrameworkC/AutomationFrameworkC/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using AutomationFrameworkC.Base_Files;
+using AutomationFrameworkC.Exercise;
 using AutomationFrameworkC.Page_Objects;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,25 @@ namespace AutomationFrameworkC
     {
         static void Main(string[] args)
         {
-            BaseTest.fnSetUp();
-            clsMercury_LoginPage objLogin = new clsMercury_LoginPage(objDriver);
+            Console.WriteLine("Constructor without parameters");
+            clsClassicCar objCCar = new clsClassicCar();
+            objCCar.fnBrake();
+            objCCar.fnParking();
+            objCCar.fnStart();
+            objCCar.fnCalculateKM();
+            objCCar.fnDisplayInfo();
+
+            Console.WriteLine("Constructor with parameters");
+            clsClassicCar objCCar2 = new clsClassicCar("A", "B", "C", "D", "E");
+            objCCar2.fnBrake();
+            objCCar2.fnParking();
+            objCCar2.fnStart();
+            objCCar2.fnCalculateKM();
+            objCCar2.fnDisplayInfo();
 
 
-            BaseTest.fnTearDown();
+            Console.ReadKey();
+
         }
     }
 }
