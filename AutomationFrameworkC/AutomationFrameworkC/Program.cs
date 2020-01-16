d8fec193b7a816a8cb04cf3012b80155e8c6d043
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2020-01-16 17:04:11 -0600

    employee class

--- AutomationFrameworkC/AutomationFrameworkC/Program.cs
+++ AutomationFrameworkC/AutomationFrameworkC/Program.cs
@@ -2,6 +2,7 @@
 using AutomationFrameworkC.Base_Files;
 using AutomationFrameworkC.Exercise;
 using AutomationFrameworkC.Exercise2;
+using AutomationFrameworkC.Exercise3;
 using AutomationFrameworkC.Page_Objects;
 using System;
 using System.Collections.Generic;
@@ -15,36 +16,44 @@ namespace AutomationFrameworkC
     {
         static void Main(string[] args)
         {
-            clsDogs objDog = new clsDogs("Poodle", "MyDog", "Howl", "Small", "Male", "Home", "Medium", 10);
-            objDog.fnDisplayInfo();
-
-            Console.WriteLine("");
-            Console.WriteLine("Constructor without parameters");
-            clsClassicCar objCCar = new clsClassicCar();
-            objCCar.fnBrake();
-            objCCar.fnParking();
-            objCCar.fnStart();
-            objCCar.fnCalculateKM();
-            objCCar.fnDisplayInfo();
-
-            Console.WriteLine("");
-            Console.WriteLine("Constructor with parameters");
-            clsClassicCar objCCar2 = new clsClassicCar("A", "B", "C", "D", "E");
-            objCCar2.fnBrake();
-            objCCar2.fnParking();
-            objCCar2.fnStart();
-            objCCar2.fnCalculateKM();
-            objCCar2.fnDisplayInfo();
 
 
-            Console.WriteLine("");
-            Console.WriteLine("Constructor with parameters");
-            clsVW objCCar3 = new clsVW("A", "B", "C", "D", "E");
-            objCCar3.fnBrake();
-            objCCar3.fnParking();
-            objCCar3.fnStart();
-            objCCar3.fnCalculateKM();
-            objCCar3.fnDisplayInfo();
+            clsEmployee objEmployee = new clsEmployee("1ABC", "Programmer", "Sr", 1000.00);
+            objEmployee.fnGetInfo();
+
+
+
+
+            //clsDogs objDog = new clsDogs("Poodle", "MyDog", "Howl", "Small", "Male", "Home", "Medium", 10);
+            //objDog.fnDisplayInfo();
+
+            //Console.WriteLine("");
+            //Console.WriteLine("Constructor without parameters");
+            //clsClassicCar objCCar = new clsClassicCar();
+            //objCCar.fnBrake();
+            //objCCar.fnParking();
+            //objCCar.fnStart();
+            //objCCar.fnCalculateKM();
+            //objCCar.fnDisplayInfo();
+
+            //Console.WriteLine("");
+            //Console.WriteLine("Constructor with parameters");
+            //clsClassicCar objCCar2 = new clsClassicCar("A", "B", "C", "D", "E");
+            //objCCar2.fnBrake();
+            //objCCar2.fnParking();
+            //objCCar2.fnStart();
+            //objCCar2.fnCalculateKM();
+            //objCCar2.fnDisplayInfo();
+
+
+            //Console.WriteLine("");
+            //Console.WriteLine("Constructor with parameters");
+            //clsVW objCCar3 = new clsVW("A", "B", "C", "D", "E");
+            //objCCar3.fnBrake();
+            //objCCar3.fnParking();
+            //objCCar3.fnStart();
+            //objCCar3.fnCalculateKM();
+            //objCCar3.fnDisplayInfo();
 
             Console.ReadKey();
 
