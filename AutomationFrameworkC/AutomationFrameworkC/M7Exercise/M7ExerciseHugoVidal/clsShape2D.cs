f2608e9b297b74dd31b977029aaeca8f2d8f87c6
vidalvader <witovidalvalencia@gmail.com>
2020-01-20 16:42:28 -0600

    Implementing the Interfaze

--- AutomationFrameworkC/AutomationFrameworkC/M7Exercise/M7ExerciseHugoVidal/clsShape2D.cs
+++ AutomationFrameworkC/AutomationFrameworkC/M7Exercise/M7ExerciseHugoVidal/clsShape2D.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace AutomationFrameworkC.M7Exercise.M7ExerciseHugoVidal
 {
-    class clsShape2D : IShape 
+    class clsShape2D : IShape
     {
+        public double dblArea { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double dblPerimeter { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
+        public void fnDisplayInfo()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
