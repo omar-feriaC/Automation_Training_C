3714067cebc0f1e71e15c4ae43fc51af68c8ac2c
vidalvader <witovidalvalencia@gmail.com>
2020-01-20 16:37:55 -0600

    Adding Interface

---
+++ AutomationFrameworkC/AutomationFrameworkC/M7Exercise/M7ExerciseHugoVidal/IShape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFrameworkC.M7Exercise.M7ExerciseHugoVidal
+{
+    interface IShape
+    {
+        //ATTRIBUTES
+        double dblArea { get; set; }
+        double dblPerimeter { get; set; }
+
+        //METHODS
+        void fnDisplayInfo();
+
+
+    }
+}
