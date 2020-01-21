0d2ceec8fecd08fb3c383cb737543eda9fe9da9b
Saul <saul.garcia@4thsource.com>
2020-01-21 12:10:23 -0600

    Part 1_A

---
+++ AutomationFrameworkC/AutomationFrameworkC/Base Files/2D_Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFrameworkC.Base_Files
+{
+    class _2D_Shape
+    {
+        //Constructor
+        public cls2DShapes() { }
+
+        public cls2DShapes(string pstrName, double pdblArea, double pdblPerimeter)
+        {
+            strName = undefined;
+            dblArea = 0;
+            dblPerimeter = 0;
+
+        }
+    }
+}
---
+++ AutomationFrameworkC/AutomationFrameworkC/Base Files/3D_Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFrameworkC.Base_Files
+{
+    class _3D_Shape
+    {
+        //Constructor
+        public cls3DShapes() { }
+
+        public cls3DShapes(string pstrName, double pdblVolume, double pdblBaseArea, double pdblPerimeter)
+        {
+            strName = undefined;
+            dblVolume = 0;
+            dblBaseArea = 0;
+            dblPerimeter = 0;
+        }
+}
