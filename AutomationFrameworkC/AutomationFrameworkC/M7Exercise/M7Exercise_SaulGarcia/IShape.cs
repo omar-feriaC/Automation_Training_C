2542a32476fd98b7d93b7885c24328a5ed216606
Saul <saul.garcia@4thsource.com>
2020-01-21 11:01:58 -0600

    Part 1

---
+++ AutomationFrameworkC/AutomationFrameworkC/M7Exercise/M7Exercise_SaulGarcia/IShape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFrameworkC.M7Exercise.M7Exercise_SaulGarcia
+{
+    interface IShape
+    {
+        //Attributes
+        double dblArea { get; set; }
+        double dblPerimeter { get; set; }
+
+        //Methods
+        void fnDisplayInfo();
+
+    }
+}
