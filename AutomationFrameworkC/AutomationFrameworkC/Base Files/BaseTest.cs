b4cf562eb280707e6df610046c7f8e9efd5d2ff1
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2020-01-24 16:52:04 -0600

    POM1

--- AutomationFrameworkC/AutomationFrameworkC/Base Files/BaseTest.cs
+++ AutomationFrameworkC/AutomationFrameworkC/Base Files/BaseTest.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -14,15 +15,18 @@ namespace AutomationFrameworkC.Base_Files
         //Variables
         public static IWebDriver objDriver;
         private static readonly string strUrl = ConfigurationManager.AppSettings.Get("url");
-
+        public static readonly string strUser = ConfigurationManager.AppSettings.Get("user");
+        public static readonly string strPass = ConfigurationManager.AppSettings.Get("password");
 
         //Functions
+        [SetUp]
         public static void fnSetUp()
         {
             objDriver = new ChromeDriver();
             objDriver.Url = strUrl;
         }
 
+        [TearDown]
         public static void fnTearDown()
         {
             objDriver.Close();
---
+++ AutomationFrameworkC/AutomationFrameworkC/Page Objects/clsMercury_RegisterPage.cs
@@ -0,0 +1,14 @@
+﻿using AutomationFrameworkC.Base_Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFrameworkC.Page_Objects
+{
+    class clsMercury_RegisterPage : BaseTest
+    {
+
+    }
+}
