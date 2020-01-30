12f212ed9d88bbeb4e833e53eaf9d07e7ce663ca
emendoza4th <47667730+emendoza4th@users.noreply.github.com>
2020-01-30 17:09:39 -0600

    M8 part 1 - 80%

---
+++ AutomationFrameworkC/AutomationFrameworkC/Page Objects/clsLinkedIn_LoginPage.cs
@@ -0,0 +1,106 @@
+﻿using AutomationFrameworkC.Base_Files;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFrameworkC.Page_Objects
+{
+    class clsLinkedIn_LoginPage : BaseTest
+    {
+        //Variables
+        private static WebDriverWait _objWait;
+        private static IWebDriver _objDriver;
+
+        //Constructor
+        public clsLinkedIn_LoginPage(IWebDriver pobjDriver)
+        {
+            _objDriver = pobjDriver;
+            _objWait = new WebDriverWait(_objDriver, new TimeSpan(0, 0, 30));
+        }
+
+        //Locators Definition
+        readonly static string STR_USERNAME = "username";
+        readonly static string STR_PASSWORD = "password";
+        readonly static string STR_SIGNIN = ".//button[@aria-label='Sign in']";
+
+        //WebElements Definition
+        private static IWebElement objUserNameTxt => _objDriver.FindElement(By.Id(STR_USERNAME));
+        private static IWebElement objPasswordTxt => _objDriver.FindElement(By.Id(STR_PASSWORD));
+        private static IWebElement objSignInBtn => _objDriver.FindElement(By.XPath(STR_SIGNIN));
+
+        //Methods
+
+        //User name text box
+        private IWebElement fnGetUserName()
+        {
+            return objUserNameTxt;
+        }
+
+        public static void fnEnterUserName(string pstrUserName)
+        {
+            try
+            {
+                _objWait.Until(ExpectedConditions.ElementExists(By.Id(STR_USERNAME)));
+                _objWait.Until(ExpectedConditions.ElementIsVisible(By.Id(STR_USERNAME)));
+                objUserNameTxt.Clear();
+                objUserNameTxt.SendKeys(pstrUserName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The element username does not exist. Error code: " + e.Message);
+                Assert.Fail();
+            }
+
+        }
+
+        //Password text box
+        private IWebElement fnGetPassword()
+        {
+            return objPasswordTxt;
+        }
+
+        public static void fnEnterPassword(string pstrPassword)
+        {
+            try
+            {
+                _objWait.Until(ExpectedConditions.ElementExists(By.Name(STR_PASSWORD)));
+                _objWait.Until(ExpectedConditions.ElementIsVisible(By.Name(STR_PASSWORD)));
+                objPasswordTxt.Clear();
+                objPasswordTxt.SendKeys(pstrPassword);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The element password does not exist. Error code: " + e.Message);
+                Assert.Fail();
+            }
+        }
+
+        //Sign in button
+        private IWebElement fnGetSignIn()
+        {
+            return objSignInBtn;
+        }
+
+        public static void fnClickSign()
+        {
+            try
+            {
+                _objWait.Until(ExpectedConditions.ElementExists(By.XPath(STR_SIGNIN)));
+                _objWait.Until(ExpectedConditions.ElementIsVisible(By.XPath(STR_SIGNIN)));
+                objSignInBtn.Click();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The element signIn does not exist. Error code: " + e.Message);
+                Assert.Fail();
+            }
+        }
+
+    }
+}
