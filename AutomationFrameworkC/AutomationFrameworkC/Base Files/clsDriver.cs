d38ebcb14160fc5b90c038c87ab688baa668479e
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2020-02-19 08:30:44 -0600

    M9Exercise

---
+++ AutomationFrameworkC/AutomationFrameworkC/Base Files/clsDriver.cs
@@ -0,0 +1,88 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutomationFrameworkC.Base_Files
+{
+    class clsDriver : BaseTest
+    {
+        /*ATTRIBUTES*/
+        private static IWebDriver _objDriver;
+        private static WebDriverWait _driverWait;
+        private static IWebElement objElement;
+
+        /*CONSTRUCTOR*/
+        public clsDriver(IWebDriver pobjDriver)
+        {
+            _objDriver = pobjDriver;
+            _driverWait = new WebDriverWait(_objDriver, new TimeSpan(0, 0, 40));
+        }
+
+        /*METHODS*/
+        private static IWebElement WaitForElementThread(IWebDriver pobjDriver, By by, string pstrDesc)
+        {
+            Thread.Sleep(5000);
+            objElement = pobjDriver.FindElement(by);
+            return objElement;
+        }
+
+        private static IWebElement fnWaitForElementDriver(IWebDriver pobjDriver, By by)
+        {
+            objElement = _driverWait.Until(ExpectedConditions.ElementExists(by));
+            objElement = _driverWait.Until(ExpectedConditions.ElementIsVisible(by));
+            return objElement;
+        }
+
+        private static IWebElement WaitForElementDriverFluent(IWebDriver pobjDriver, By by)
+        {
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(objDriver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(5);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(250);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            IWebElement searchResult = fluentWait.Until(x => x.FindElement(By.Id("search_result")));
+            return objElement;
+        }
+
+        /*Wait for element to Exist*/
+        public static IWebElement fnWaitForElementToExist(By by)
+        {
+            objElement = _driverWait.Until(ExpectedConditions.ElementExists(by));
+            return objElement;
+        }
+
+        /*Wait for element to be visible*/
+        public static IWebElement fnWaitForElementToBeVisible(By by)
+        {
+            objElement = _driverWait.Until(ExpectedConditions.ElementIsVisible(by));
+            return objElement;
+        }
+
+        /*Wait for element to be clickable*/
+        public static IWebElement fnWaitForElementToBeClickable(By by)
+        {
+            objElement = _driverWait.Until(ExpectedConditions.ElementToBeClickable(by));
+            return objElement;
+        }
+
+        /*Wait for element to be selected*/
+        public static bool fnWaitForElementToBeSelected(By by)
+        {
+            if (_driverWait.Until(ExpectedConditions.ElementToBeSelected(by)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ AutomationFrameworkC/AutomationFrameworkC/Test Cases/Test_PHPTravels.cs
@@ -0,0 +1,34 @@
+﻿using AutomationFrameworkC.Base_Files;
+using AutomationFrameworkC.Page_Objects;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFrameworkC.Test_Cases
+{
+    class Test_PHPTravels : BaseTest
+    {
+        clsPHPTravels_LoginPage objPHP;
+
+
+        [Test]
+        public void Test_M9Exercise()
+        {
+            //Init objects
+            objPHP = new clsPHPTravels_LoginPage(objDriver);
+            //Login Action
+            Assert.AreEqual(true, objDriver.Title.Contains("Administrador Login."), "The Login Page was not loaded correctly.");
+            clsPHPTravels_LoginPage.fnEnterEmail("admin@phptravels.com");
+            clsPHPTravels_LoginPage.fnEnterPassword("demoadmin");
+            clsPHPTravels_LoginPage.fnClickLoginButton();
+            clsPHPTravels_LoginPage.fnWaitHamburgerMenu();
+            Assert.AreEqual(true, objDriver.Title.Contains("Dashboard."), "The Dashboard was not loaded correctly.");
+
+        }
+
+
+    }
+}
