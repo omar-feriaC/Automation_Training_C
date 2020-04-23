7e46e4d828c7429e4e2da76e9e7312bc7a5eaafb
JamaicaGold <jorgepilotzi@gmail.com>
2020-04-23 13:13:29 -0500

    changes requested on the 1:1 call

--- AutomationFrameworkC/AutomationFrameworkC/Jorge_Pilotzi_M8_Excercise/Test Cases/LinkedIn_Search_Test.cs
+++ AutomationFrameworkC/AutomationFrameworkC/Jorge_Pilotzi_M8_Excercise/Test Cases/LinkedIn_Search_Test.cs
@@ -17,67 +17,46 @@ namespace AutomationFrameworkC.Jorge_Pilotzi_M8_Excercise.Test_Cases
 {
    class LinkedIn_Search_Test : Linkedin_Test
     {
-        WebDriverWait Wait;
         LinkedIn_Search objLinkedInSrch;
-
         [Test]
         public void LinkedIn_SearchPageTest()
         {
             //Variables
+            WebDriverWait Wait;
             Wait = new WebDriverWait(driver, new TimeSpan(0, 1, 20));
             string [] arrTechnologies = { "Java", "Python", "Pega", "C#", "C Language" };
-            
             // Step 1 - Login
-
             driver.Manage().Window.Maximize();
             Linkedin_LoginPage();
-
             // Step 2 - Search
-
             objLinkedInSrch = new LinkedIn_Search(driver);
-            LinkedIn_Search.FnGetSrchFld("Java");
-            LinkedIn_Search.FnGetSrchBtn();
+            LinkedIn_Search.fnGetSrchFld("Java");
+            LinkedIn_Search.fnGetSrchBtn();
             Wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//span[text()='People' or text()='Gente']")));
-
             //Step 3 - Select People
-
-            LinkedIn_Search.FnSelectPeopleCheckbox();
-            Wait.Until(ExpectedConditions.ElementExists(By.XPath("//span[text()='People' or text()='Gente']")));
-            Wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//span[text()='People' or text()='Gente']")));
+            LinkedIn_Search.fnSelectPeopleCheckbox();
             Wait.Until(ExpectedConditions.UrlContains("people"));
-
             // Step 4 - Select Location
-
-            LinkedIn_Search.FnGetAllFiltersBtn();
+            LinkedIn_Search.fnGetAllFiltersBtn();
             Wait.Until(ExpectedConditions.ElementExists(By.XPath("//input[@aria-label='Add a country/region' or @aria-label='Añadir un país o región']")));
             Wait.Until(ExpectedConditions.ElementExists(By.XPath("//label[text()='Mexico' or text()='México']")));
-            LinkedIn_Search.FnGetRegionMexico();
-            LinkedIn_Search.FnGetAddCountry("Italy");
+            LinkedIn_Search.fnGetRegionMexico();
+            LinkedIn_Search.fnGetAddCountry("Italy");
             Wait.Until(ExpectedConditions.ElementExists(By.XPath("//div[@class='basic-typeahead__triggered-content search-s-add-facet__typeahead-tray']")));
             Wait.Until(ExpectedConditions.TextToBePresentInElement(LinkedIn_Search.GetItalyDropDown(), "Italy"));
-            LinkedIn_Search.FnGetItaly();
-
-
+            LinkedIn_Search.fnGetItaly();
             // Step 5- Select Languages
-
-            LinkedIn_Search.FnSelectLanguages();
-
-            
+            LinkedIn_Search.fnGetLanguageEnglish();
+            LinkedIn_Search.fnSelectLanguages();
             // Step 6 - Apply Filters
-
-            LinkedIn_Search.FnGetApplyBtn();
-
+            LinkedIn_Search.fnGetApplyBtn();
             // Step 7 - Look for Technologies & Print
-
             foreach (string Technologies in arrTechnologies)
             {
-                LinkedIn_Search.FnGetSrchFld(Technologies);
+                LinkedIn_Search.fnGetSrchFld(Technologies);
                 Wait.Until(ExpectedConditions.ElementExists(By.XPath("//p[@class='subline-level-1 t-14 t-black t-normal search-result__truncate']")));
-                LinkedIn_Search.FnGetTechnology();
+                LinkedIn_Search.fnGetTechnology();
             }
-
         }
-
     }
-
 }
