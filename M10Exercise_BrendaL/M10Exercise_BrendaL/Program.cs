d141c8bc7fa21582274af4e37c38b1305f79103e
Brenda Lopez Granados <brenda.lopez@agilethought.com>
2020-05-19 12:58:59 -0500

    M10Exercise_BrendaL

--- M10Exercise_BrendaL/M10Exercise_BrendaL/Program.cs
+++ M10Exercise_BrendaL/M10Exercise_BrendaL/Program.cs
@@ -33,42 +33,52 @@ namespace M10Exercise_BrendaL
             ////2. Print the size of a file in bytes in a directory using LINQ. 
             #region
 
-            //string[] dirfiles = Directory.GetFiles("C:/Test");
-            //// there are three files in the directory abcd are :             
-            //// abcd.txt, simple_file.txt and xyz.txt 
+            //string[] dirfiles = Directory.GetFiles(@"C:\Test");
+
+            ////there are three files in the directory abcd are :             
+            ////abcd.txt, simple_file.txt and xyz.txt
 
             //Console.Write("\nLINQ : Calculate the Size of File : ");
             //Console.Write("\n------------------------------------\n");
 
-            //var filesList = from selectFiles in dirfiles
+            //var filesList = from selectFiles in dirfiles 
             //                select selectFiles;
 
             //foreach (string element in filesList)
-            //{ 
-            //    Console.WriteLine($"Selected File: {element}.\tThe size of the file is: {element.Length} bites");
+            //{
+            //    string longFile = new Uri(element).LocalPath;
+            //    FileInfo fl = new FileInfo(longFile);
+
+            //    Console.WriteLine($"Selected File: {element} \tThe size of the file is: {fl.Length} bites");
             //}
             #endregion
 
             ////3. Shows how the three parts of a query operation execute. 
             #region  
-                        
-            //int[] n1 = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-            //Console.Write("\nBasic structure of LINQ : ");
-            //Console.Write("\n---------------------------");
+            int[] n1 = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-            //var values = from num in n1
-            //             where num == 5
-            //             select num;
+            Console.Write("\nBasic structure of LINQ : ");
+            Console.Write("\n---------------------------");
+
+            var values = from num in n1
+                         where num == 5
+                         select num;
+
+            foreach (int element in values)
+            {
+                string number = element.ToString();
+
+                Console.WriteLine("\nQuery:\tvar values = from num in n1");
+                Console.WriteLine("      \twhere num == 5");
+                Console.WriteLine("      \tselect num;");
+                Console.WriteLine("\n\"var values\": as the name of the variable that will allocate the results of the query.");
+                Console.WriteLine("Part 1: \"from num in n1\": num is the variable of the items, meaning from items and in n1 means is from the array n1");
+                Console.WriteLine("Part 2: \"where num == 5\": where means that the condition is that the item equals 5");
+                Console.WriteLine("Part 3: \"select num\": then the items that match the condition will be in values");
+                Console.WriteLine($"\nSince the array has the following numbers in it (0, 1, 2, 3, 4, 5, 6, 7, 8, 9). The only one that matches the condition is: {number}");
+            }
             
-            //Console.WriteLine("\nQuery:\tvar values = from num in n1");
-            //Console.WriteLine("      \twhere num == 5");
-            //Console.WriteLine("      \tselect num;");
-            //Console.WriteLine("\n\"var values\": as the name of the variable that will allocate the results of the query.");
-            //Console.WriteLine("Part 1: \"from num in n1\": num is the variable of the items, meaning from items and in n1 means is from the array n1");
-            //Console.WriteLine("Part 2: \"where num == 5\": where means that the condition is that the item equals 5");
-            //Console.WriteLine("Part 3: \"select num\": then the items that match the condition will be in values");
-            //Console.WriteLine($"\nSince the array has the following numbers in it (0, 1, 2, 3, 4, 5, 6, 7, 8, 9). The only one that matches the condition is: {values}");
 
             #endregion
 
@@ -108,9 +118,12 @@ namespace M10Exercise_BrendaL
 
             #endregion
 
-            Console.ReadLine();
 
+
+
+            Console.ReadLine();
         }
+
     }
 }
 
